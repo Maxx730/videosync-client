941fa580bc3728c645d16c3fa3cd1de37c0917a6
Maxx730 <max.kinghorn@gmail.com>
2021-06-25 08:35:17 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import socketClient from 'socket.io-client';
 
-const SERVER = process.env.NODE_ENV === 'development' ? 'localhost:4000' : 'https://videosync-core.herokuapp.com/';
+const SERVER = process.env.NODE_ENV === 'development' ? 'localhost:4000' : 'https://videosync-core-kzoyq.ondigitalocean.app';
 
 function App() {
   const socket = socketClient(SERVER);
   return (
     <div className="App">
-
+dafdasf
     </div>
   );
 }
