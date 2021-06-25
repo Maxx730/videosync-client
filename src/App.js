3efab2defc6b69d05c3cbbcdca18775ecec1e047
Maxx730 <max.kinghorn@gmail.com>
2021-06-25 06:17:17 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import socketClient from 'socket.io-client';
+
+const SERVER = process.env.NODE_ENV === 'development' ? 'localhost:4000' : 'https://videosync-core.herokuapp.com/';
 
 function App() {
+  const socket = socketClient(SERVER);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
